b25fa5d1ad8dc90bb37a44bfdd61a2f71facefcd
Danucosuko suko <71569318+Danucosukosuko@users.noreply.github.com>
2023-09-05 19:45:57 +0200

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,26 @@
+# -*- coding: latin-1 -*-
+import socket
+import subprocess
+import os
+
+def start_client():
+    host = '192.168.24.153'  # Cambia la dirección IP a la del servidor
+    port = 4444
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+
+    while True:
+        command = input(f"Shell remota ({host}:{port})> ")
+        client.send(command.encode())
+
+        if command.lower() == 'exit':
+            break
+
+        response = client.recv(4096).decode()
+        print(response)
+
+    client.close()
+
+if __name__ == "__main__":
+    start_client()
